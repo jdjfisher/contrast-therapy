26d8977c90123cd4a174feb1f173fa0e25a138fb
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2025-10-20 22:34:37 +0100

    Added about section

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 import '@/index.css';
 import Timer from '@/components/Timer.vue';
 import Settings from '@/components/Settings.vue';
+import About from '@/components/About.vue';
 import { ref } from 'vue';
 
-const showSettings = ref(false);
+const panel = ref<'timer' | 'settings' | 'about'>('timer');
 </script>
 
 <template>
@@ -13,21 +14,40 @@ const showSettings = ref(false);
       <div class="flex items-center justify-between gap-2">
         <h1 class="mb-1 font-mono text-xs font-bold">Contrast Therapy</h1>
 
-        <button
-          class="icon-button cursor-pointer text-gray-500 transition-colors hover:text-gray-700"
-          @click="showSettings = !showSettings"
-          :title="showSettings ? 'Back to Timer' : 'Settings'"
-        >
-          <i class="material-icons text-[16px]!">
-            {{ showSettings ? 'arrow_back' : 'settings' }}
-          </i>
-        </button>
+        <transition name="fade" mode="out-in">
+          <div class="flex gap-2" v-if="panel === 'timer'">
+            <button
+              class="icon-button cursor-pointer p-1 text-gray-500 transition-colors hover:text-gray-700"
+              @click="panel = 'about'"
+              title="About"
+            >
+              <i class="material-icons text-[16px]!"> help </i>
+            </button>
+            <button
+              class="icon-button cursor-pointer p-1 text-gray-500 transition-colors hover:text-gray-700"
+              @click="panel = 'settings'"
+              title="Settings"
+            >
+              <i class="material-icons text-[16px]!"> settings </i>
+            </button>
+          </div>
+
+          <button
+            v-else
+            class="icon-button cursor-pointer p-1 text-gray-500 transition-colors hover:text-gray-700"
+            @click="panel = 'timer'"
+            title="Back to Timer"
+          >
+            <i class="material-icons text-[16px]!"> arrow_back </i>
+          </button>
+        </transition>
       </div>
 
       <div class="grid h-96 w-80 place-items-center rounded-lg border p-12 shadow">
         <transition name="fade" mode="out-in">
-          <Settings v-if="showSettings" />
-          <Timer v-else :cold-duration="60" :hot-duration="120" :cycles="5" />
+          <Timer v-if="panel === 'timer'" :cold-duration="60" :hot-duration="120" :cycles="5" />
+          <Settings v-else-if="panel === 'settings'" />
+          <About v-else />
         </transition>
       </div>
     </div>
---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h2 class="mb-2 text-lg font-bold">Contrast Therapy</h2>
+    <p class="mb-2 text-gray-500">ˈkɒntrɑːst ˈθɛrəpi</p>
+
+    <hr class="mb-4" />
+
+    <p class="text-sm">
+      Contrast therapy, or hot and cold therapy, alternates between heat and cold to aid recovery.
+      Heat relaxes muscles and boosts circulation, while cold reduces inflammation and soreness.
+      Together, they can improve recovery, circulation, and pain relief after workouts or injury.
+    </p>
+  </div>
+</template>
